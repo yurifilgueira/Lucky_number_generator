6c4763c492fc4df79d607f379d40e169aa6c5d4b
yurifilgueira <yurimedeiros141@gmail.com>
2023-01-24 15:55:42 -0300

    Implemented lucky numbers generator method

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,19 @@
 package application;
 
+import java.util.List;
+
+import entities.NumberGenerator;
+
 public class Program {
 
 	public static void main(String[] args) {
-
 		
+		List<Integer> luckyNumbers = NumberGenerator.generateLuckyNumbers();
+		
+		for(int i = 0; i < 6; i++) {
+			System.out.print(luckyNumbers.get(i));
+			if (i < 5) System.out.print("-");
+		}
 
 	}
 
--- src/entities/NumberGenerator.java
+++ src/entities/NumberGenerator.java
@@ -3,11 +3,16 @@ package entities;
 import java.util.ArrayList;
 import java.util.List;
 
-public class NumberGenerator {
-
-	private Integer number;
-	
-	List<Integer> numbers = new ArrayList<>();
-	
-	
+public  class NumberGenerator {
+	 
+	public static List<Integer> generateLuckyNumbers(){
+		List<Integer> luckyNumbers = new ArrayList<>();
+		
+		for (int i = 0; i < 6; i++) {
+			Long numb = Math.round(Math.random() * 60);
+			luckyNumbers.add(numb.intValue());
+		}
+		
+		return luckyNumbers;
+	}	
 }
