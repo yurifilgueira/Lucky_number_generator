f9c9cffdba3cd4b9872b2ece0b1212b04bc4e147
yurifilgueira <yurimedeiros141@gmail.com>
2023-01-23 23:33:15 -0300

    Project created and NumberGenerator class created

---
+++ src/entities/NumberGenerator.java
@@ -0,0 +1,13 @@
+package entities;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class NumberGenerator {
+
+	private Integer number;
+	
+	List<Integer> numbers = new ArrayList<>();
+	
+	
+}
